a55cceb4ef6d5c0fbf145cd59ae07f1bbef05255
hieu <hieuvnautotech@gmail.com>
2022-08-22 15:27:30 +0700

    hoàn thành ở cty

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,12 +6,13 @@ $("#update_user").submit(function (event) {
   event.preventDefault();
 
   var unindexed_array = $(this).serializeArray();
+  // console.log(unindexed_array);
+
   var data = {};
 
   $.map(unindexed_array, function (n, i) {
     data[n["name"]] = n["value"];
   });
-
   var request = {
     url: `http://localhost:3000/api/users/${data.id}`,
     method: "PUT",
